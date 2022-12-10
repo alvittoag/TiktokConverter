25d66ba5b317e067265e3a259d6cac096dfa3309
alvittoag <alvittogenz09@gmail.com>
2022-12-10 22:39:31 +0700

    Update font size

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div>
       <div className="text-center flex flex-col gap-2 mt-7 lg:mt-14 h-max">
-        <h1 className="font-bold text-2xl text-slate-700 lg:text-4xl">
+        <h1 className="font-bold text-xl text-slate-700 lg:text-4xl">
           Download Video Tiktok
         </h1>
         <p className="text-slate-700">
